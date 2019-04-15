ce80843581895359f86c9b662b928772330c9fa3
Minatula <minatula.shakhbazov@gmail.com>
2019-04-15 18:14:22 +0800

    index.php changed

--- yii/web/index.php
+++ yii/web/index.php
@@ -1,11 +1,9 @@
 <?php
 
-use yii\web\Application;
-
 define('YII_DEBUG', true);
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/yii2.php';
-$yii = new Application($config);
+$yii = new \yii\web\Application($config);
 $yii->run();
